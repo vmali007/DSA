cf49e2948e50ef66c07b169e272dfddd0309ce1d
Vijay Mali <50293032+vmali007@users.noreply.github.com>
2023-01-09 09:11:27 +0530

    Update Repeat_and_Missing_Number.cpp

--- Repeat_and_Missing_Number.cpp
+++ Repeat_and_Missing_Number.cpp
@@ -3,22 +3,22 @@
 
 pair<int,int> missingAndRepeating(vector<int> &arr, int n)
 {
-    vector<int> count(n-1,0);
+    vector<int> count(n,0);
     pair<int,int> ans;
 
     for(int i=0;i<n;i++){
-        count[arr[i+1]]++;
+        count[arr[i]-1]++;
     }
-
-    for(int i =0;i<n-1;i++){
+    
+    for(int i =0;i<n;i++){
         if(count[i] == 0){
-            ans.fisrt = i+1;    
+            ans.first = i+1;    
         }
 
         if(count[i] == 2){
-            ans.second = i+1
+            ans.second = i+1;
             }
     }
     return ans;
-	
+    
 }
